d442c06c844f49104ce2daacad834c014e55b0bc
Joe Feyen <76495851+jfeyen4249@users.noreply.github.com>
2024-03-06 13:37:24 -0600

    Menu update

--- public/menu.js
+++ public/menu.js
@@ -3,6 +3,7 @@ let menu = document.getElementById('menu')
 menu.innerHTML = `
 <li class="mb-2"><a href="#" class="text-white hover:text-blue-400">Dashboard</a></li>
 <li class="mb-2"><a href="/status" class="text-white hover:text-blue-400">System Status</a></li>
+<li class="mb-2"><a href="#" class="text-white hover:text-blue-400">Contacts</a></li>
 <li class="mb-2"><a href="/docs" class="text-white hover:text-blue-400">Documentation</a></li>
 <li class="mb-2"><a href="/inventory" class="text-white hover:text-blue-400">Inventory</a></li>
 <li class="mb-2"><a href="/passwords" class="text-white hover:text-blue-400">Passwords</a></li>
