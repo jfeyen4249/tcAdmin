c6945a1838abab36f5c50e011d95d5cd8cdf78c8
Joe Feyen <76495851+jfeyen4249@users.noreply.github.com>
2024-03-29 12:52:40 -0500

    Menu Update
    Added Maps

--- public/menu.js
+++ public/menu.js
@@ -10,6 +10,7 @@ menu.innerHTML = `
 <li class="mb-2"><a href="/news" class="text-white hover:text-blue-400">News</a></li>
 <li class="mb-2"><a href="/passwords" class="text-white hover:text-blue-400">Passwords</a></li>
 <li class="mb-2"><a href="/staff" class="text-white hover:text-blue-400">Staff Directory</a></li>
+<li class="mb-2"><a href="/maps" class="text-white hover:text-blue-400">Maps</a></li>
 <li class="mb-2"><a href="/wifi" class="text-white hover:text-blue-400">Wifi Passwords</a></li>
 <li class="mb-2"><a href="/settings" class="text-white hover:text-blue-400">Settings</a></li>
 <li class="mb-2"><a href="#" class="text-white hover:text-blue-400" onclick="logout()">Logout</a></li>
